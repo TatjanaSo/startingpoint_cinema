a363ddd77cf595ca39f7af2dbe8df67d0fd7240f
TatjanaSo <tatjana-sohns@gmx.de>
2017-10-20 10:07:44 +0200

    update

--- Batches/jenkins_unittest.bat
+++ Batches/jenkins_unittest.bat
@@ -1,4 +1,4 @@
-Tatjana_starting\StartingPoint_2005_0\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe
+StartingPoint_2005_0\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe
 Test_Starting\bin\Deug\Test_Starting.dll 
 --result=TestResult.xml 
 
