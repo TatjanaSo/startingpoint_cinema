78f51550176ff4e2731249ff30b375524cbe69a2
TatjanaSo <tatjana-sohns@gmx.de>
2017-10-20 09:59:44 +0200

    Batches/jenkins_unittest.bat updated

--- Batches/jenkins_unittest.bat
+++ Batches/jenkins_unittest.bat
@@ -1,3 +1,3 @@
 StartingPoint_2005_0\packages\NUnit.ConsoleRunner.3.7.0\tools\tools\nunit3-console.exe 
-Test_StartingPoint\bin\Deug\Test_StartingPoint.dll 
+Test_Starting\bin\Deug\Test_Starting.dll 
 --result=TestResult.xml 
